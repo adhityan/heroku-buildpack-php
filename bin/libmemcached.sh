134ca171154832250a46ad34ab610cd89988a813
Adhityan <adhityan@hotmail.com>
2016-05-11 16:37:28 +0530

    lol

---
+++ bin/libmemcached.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "-----> Installing Libmemcached..."
+wget http://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
+tar -zxvf libmemcached-1.0.18.tar.gz
+cd libmemcached-1.0.18
+./configure
+make && make install
+pecl install memcached
