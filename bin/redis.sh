8642117717af0d0a5c80a2ecc7f328e2da4878ff
Adhityan <adhityan@hotmail.com>
2016-05-11 14:08:28 +0530

    new redis install flow

---
+++ bin/redis.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+dep_url=https://github.com/phpredis/phpredis.git
+redis_dir=phpredis
+echo "-----> Installing Redis..."
+
+### Redis
+echo "[LOG] Downloading Redis"
+git clone $dep_url -q
+if [ ! -d "$redis_dir" ]; then
+  echo "[ERROR] Failed to find php redis directory $redis_dir"
+  exit
+fi
+
+cd $redis_dir
+git checkout php7
+phpize
+./configure
+make && make install
+
+# /app/php/bin/phpize
+# ./configure --enable-phalcon --with-php-config=$PHP_ROOT/bin/php-config
+# make
+# make install
+
+echo "important extension redis into php.ini"
+echo "extension=redis.so" >> /app/.heroku/php/etc/php/php.ini
